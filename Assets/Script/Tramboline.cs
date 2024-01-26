02bf1e79aabd9c41ecfcfb1dcfad43df8d08a91b
SoftRiseGames <98230446+SoftRiseGames@users.noreply.github.com>
2024-01-27 02:33:43 +0300

    LAST

---
+++ Assets/Script/Tramboline.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tramboline : MonoBehaviour
+{
+    [SerializeField] int bounce;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Character")
+            collision.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * bounce ,ForceMode2D.Impulse);
+    }
+}
