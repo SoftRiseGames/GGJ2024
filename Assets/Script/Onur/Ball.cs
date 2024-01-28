fb2a3cec313004a4605952faf5a69252dfec69a9
Hypsenor <haleko123@gmail.com>
2024-01-28 15:02:51 +0300

    a

--- Assets/Script/Onur/Ball.cs
+++ Assets/Script/Onur/Ball.cs
@@ -7,12 +7,14 @@ using Cinemachine;
 
 public class Ball : MonoBehaviour
 {
+    public AudioSource audioSource;
     public float speed = 5f;
     public Slider slider;
     public CinemachineImpulseSource cinemachineImpulseSource;
     void Start()
     {
         slider = GameObject.Find("Canvas").transform.GetChild(0).GetComponent<Slider>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     void Update()
@@ -29,11 +31,12 @@ public class Ball : MonoBehaviour
     {
         if (collision.gameObject.tag == "Character")
         {
+            audioSource.Play();
             slider.value += 30;
             cinemachineImpulseSource.GenerateImpulse();
             Destroy(this.gameObject);
             GameObject.Find("Character").GetComponent<CharacterMovement>().speed = 500;
-            Destroy(this.gameObject, 7);
+            Destroy(this.gameObject, 7);      
         }
     }
 }
